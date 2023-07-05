bea87b9504a665406dc85c470ee238f5710a137d
Martin Belko <martbelko@gmail.com>
2023-07-06 01:04:51 +0200

    Working a bit on the actual frontend and registration

--- wa-app/src/Components/Register/Register.tsx
+++ wa-app/src/Components/Register/Register.tsx
@@ -1,7 +1,19 @@
 import './Register.scss';
 
-import { FormEvent, useEffect, useId, useRef, useState } from 'react';
+import { FormEvent, useEffect, useId, useState } from 'react';
 import axios from 'axios';
+
+import checkIcon from './../../assets/icons/check.svg';
+import crossIcon from './../../assets/icons/cross.svg';
+
+interface AccountTakenPayload {
+	usernameTaken: boolean;
+	emailTaken: boolean;
+}
+
+enum DataState {
+	None, Loading, Ok, NotOk, Invalid
+}
 
 export default function Register(): JSX.Element {
 	const usernameId = useId();
@@ -14,13 +26,54 @@ export default function Register(): JSX.Element {
 	const [password, setPassword] = useState('');
 	const [passwordRepeat, setPasswordRepeat] = useState('');
 
+	const [usernameDataState, setUsernameDataState] = useState(DataState.None);
+	const [emailDataState, setEmailDataState] = useState(DataState.None);
+
+	const [submitEnabled, setSubmitEnabled] = useState(false);
+
+	const getClassModifierFromDataState = (dataState: DataState) => {
+		switch (dataState) {
+		case DataState.Loading:
+			return 'loading';
+		case DataState.Ok:
+			return 'ok';
+		case DataState.NotOk:
+		case DataState.Invalid:
+			return 'notok';
+		}
+
+		return '';
+	};
+
+	const checkSubmitEnable = () => {
+		if (usernameDataState === DataState.Ok && emailDataState === DataState.Ok && password === passwordRepeat) {
+			setSubmitEnabled(true);
+		} else {
+			setSubmitEnabled(false);
+		}
+	};
+
 	useEffect(() => {
+		if (username.length === 0) {
+			setUsernameDataState(DataState.None);
+		} else {
+			setUsernameDataState(DataState.Loading);
+		}
+
 		const delayDebounceFn = setTimeout(() => {
+			if (username.length === 0) {
+				setUsernameDataState(DataState.None);
+				return;
+			}
+
 			axios.post('https://localhost:5000/AccountTaken', {
-				username: 'abc',
-				email: 'email'
+				username: username
 			}).then(res => {
-				console.log(res.data);
+				if (res != null && res.data != null) {
+					const payload = res.data as AccountTakenPayload;
+					setUsernameDataState(payload.usernameTaken ? DataState.NotOk : DataState.Ok);
+					checkSubmitEnable();
+				}
 			});
 		}, 1500);
 
@@ -28,33 +81,125 @@ export default function Register(): JSX.Element {
 	}, [username]);
 
 	useEffect(() => {
+		if (email.length === 0) {
+			setEmailDataState(DataState.None);
+		} else {
+			setEmailDataState(DataState.Loading);
+		}
+
 		const delayDebounceFn = setTimeout(() => {
-			// TODO: Query GraphQL if this username already exists
+			if (email.length === 0) {
+				setEmailDataState(DataState.None);
+				return;
+			}
+
+			// TODO: Use email regex
+			if (email.length < 3 || !email.includes('@')) {
+				setEmailDataState(DataState.Invalid);
+				return;
+			}
+
+			axios.post('https://localhost:5000/AccountTaken', {
+				email: email
+			}).then(res => {
+				if (res != null && res.data != null) {
+					const payload = res.data as AccountTakenPayload;
+					setEmailDataState(payload.emailTaken ? DataState.NotOk : DataState.Ok);
+					checkSubmitEnable();
+				}
+			});
 		}, 1500);
 
 		return () => clearTimeout(delayDebounceFn);
 	}, [email]);
 
+	useEffect(() => {
+		if (password.length > 0 && passwordRepeat.length > 0) {
+			checkSubmitEnable();
+		}
+	}, [password, passwordRepeat]);
+
 	const onRegisterSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+	};
+
+	const usernameClassModifier = usernameDataState === DataState.None ? '' : getClassModifierFromDataState(usernameDataState);
+	const usernameInputClass = usernameDataState === DataState.None ? '' : `register-form__input--${usernameClassModifier}`;
+	const usernameLabelClass = usernameDataState === DataState.None ? '' : `register-form__label--${usernameClassModifier}`;
+
+	const emailClassModifier = emailDataState === DataState.None ? '' : getClassModifierFromDataState(emailDataState);
+	const emailInputClass = emailDataState === DataState.None ? '' : `register-form__input--${emailClassModifier}`;
+	const emailLabelClass = emailDataState === DataState.None ? '' : `register-form__label--${emailClassModifier}`;
+
+	const getUsernameDescription = () => {
+		switch (usernameDataState) {
+		case DataState.Ok:
+			return <img src={checkIcon} alt='check' />;
+		case DataState.NotOk:
+			return (
+				<>
+					<img src={crossIcon} alt='cross' />Invalid
+				</>);
+		case DataState.Loading:
+			return 'Loading';
+		case DataState.Invalid:
+			return 'Invalid';
+		}
+
+		return '';
 	};
 
 	return (
 		<div className='register-container'>
 			<form className='register-form' onSubmit={onRegisterSubmit}>
-				<label className='register-form__label--ok' htmlFor={usernameId}>Username</label>
-				<input type='text' className='register-form__input--ok' id={usernameId} onChange={(e) => setUsername(e.target.value)} />
+				<div className='register-form__element'>
+					<label className={usernameLabelClass} htmlFor={usernameId}>Username</label>
+					<div className='register-form__element__input-container'>
+						<input
+							minLength={4}
+							maxLength={30}
+							type='text'
+							className={usernameInputClass}
+							id={usernameId}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+						<span className='register-form__description'>
+							{getUsernameDescription()}
+						</span>
+					</div>
+				</div>
 
-				<label htmlFor={emailId}>E-mail</label>
-				<input type='email' id={emailId} onChange={(e) => setEmail(e.target.value)} />
+				<div className='register-form__element'>
+					<label className={emailLabelClass} htmlFor={emailId}>E-mail</label>
+					<div className='register-form__element__input-container'>
+						<input type='email' minLength={3} maxLength={320} className={emailInputClass} id={emailId} onChange={(e) => setEmail(e.target.value)} />
+						<span className='register-form__description'>
+							{''}
+						</span>
+					</div>
+				</div>
 
-				<label htmlFor={passwordId}>Password</label>
-				<input type='password' id={passwordId} onChange={(e) => setPassword(e.target.value)} />
+				<div className='register-form__element'>
+					<label htmlFor={passwordId}>Password</label>
+					<div className='register-form__element__input-container'>
+						<input type='password' minLength={8} maxLength={128} id={passwordId} onChange={(e) => setPassword(e.target.value)} />
+						<span className='register-form__description'>
+							{''}
+						</span>
+					</div>
+				</div>
 
-				<label htmlFor={repeatPasswordId}>Repeat password</label>
-				<input type='password' id={repeatPasswordId} onChange={(e) => setPasswordRepeat(e.target.value)} />
+				<div className='register-form__element'>
+					<label htmlFor={repeatPasswordId}>Repeat password</label>
+					<div className='register-form__element__input-container'>
+						<input type='password' minLength={8} maxLength={128} id={repeatPasswordId} onChange={(e) => setPasswordRepeat(e.target.value)} />
+						<span className='register-form__description'>
+							{''}
+						</span>
+					</div>
+				</div>
 
-				<button type='submit'>Submit</button>
+				<button disabled={!submitEnabled} type='submit'>Submit</button>
 			</form>
 		</div>
 	);
